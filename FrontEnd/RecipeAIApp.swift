78eef504baa7d752f90300e1412f8f3babaf2a58
asaini987 <135754860+asaini987@users.noreply.github.com>
2023-07-04 15:48:52 -0400

    Create RecipeAIApp.swift

---
+++ FrontEnd/RecipeAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeAIApp.swift
+//  RecipeAI
+//
+//  Created on 6/27/23.
+//
+
+import SwiftUI
+
+@main
+struct RecipeAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LoginView()
+        }
+    }
+}
