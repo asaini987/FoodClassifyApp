bddef21b5ed760e9478b8ac8f9661a55bb409a53
asaini987 <135754860+asaini987@users.noreply.github.com>
2024-01-27 20:12:49 -0500

    Update AuthViewModel.swift

--- FrontEnd/Authentication/AuthViewModel.swift
+++ FrontEnd/Authentication/AuthViewModel.swift
@@ -8,14 +8,41 @@ class AuthViewModel: ObservableObject {
         registerAuthStateHandler()
     }
     
+    let currentUser = Auth.auth().currentUser
     @Published var user: User?
     @Published var validAuth = false
+    
     
     private var authStateHandle: AuthStateDidChangeListenerHandle?
     
     func registerAuthStateHandler() {
         authStateHandle = Auth.auth().addStateDidChangeListener { auth, user in
             self.user = user
+            Task {
+                await self.addUser(userId: user!.uid)
+            }
+        }
+    }
+    
+    func addUser(userId: String) async {
+        guard let jsonData = try? JSONSerialization.data(withJSONObject: ["userId" : userId]) else {
+            return
+        }
+        
+        guard let url = URL(string: "http://example_API/insert_user") else {
+                return
+        }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = jsonData
+        
+        // Sending request and decoding response
+        do {
+            let _ = try await URLSession.shared.upload(for: request, from: jsonData)
+        } catch {
+            print(error.localizedDescription)
         }
     }
     
